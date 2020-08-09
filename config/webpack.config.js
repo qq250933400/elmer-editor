bf8a60040f235ca1af68b1538adf976e3c71ac5f
莫书江 <moshujiang@moshujiangdeMacBook-Pro.local>
2020-08-09 11:22:56 +0800

    update webpack config for monaco editor

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,26 +1,9 @@
 const monacoEditorWebpackPlugin = require("monaco-editor-webpack-plugin");
-const MonacoWebpackPlugin = require('monaco-editor-esm-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const path = require("path");
 module.exports = {
     plugins: [
         new monacoEditorWebpackPlugin(),
-        // new MonacoWebpackPlugin()
     ],
-    // module: {
-    //     rules: [
-    //         {
-	// 			test: /\.js/,
-	// 			enforce: 'pre',
-	// 			include: /node_modules[\\\/]monaco-editor[\\\/]esm/,
-	// 			use: [
-    //                 {
-    //                     loader: path.resolve(__dirname, "./webpack.loader.js")
-    //                 }
-    //             ]
-	// 		},
-    //     ]
-    // },
     optimization: {
         minimize: true,
         minimizer: [new TerserPlugin()]
